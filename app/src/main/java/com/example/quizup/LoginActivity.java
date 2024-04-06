952c3104ce3051e573336999466ea4ad4de2fc14
Riana <simonyanriana@gmail.com>
2024-04-06 12:15:56 +0400

    added TestActivity

---
+++ app/src/main/java/com/example/quizup/LoginActivity.java
@@ -0,0 +1,142 @@
+package com.example.quizup;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Dialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.identity.BeginSignInRequest;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText email, pass;
+    private Button loginB;
+    TextView forgotPassB, signupB;
+    private FirebaseAuth mAuth;
+    private Dialog progressDialog;
+    private TextView dialogText;
+    private RelativeLayout gSignB;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        email = findViewById(R.id.email);
+        pass = findViewById(R.id.password);
+        loginB = findViewById(R.id.loginB);
+        forgotPassB = findViewById(R.id.forgot_pass);
+        signupB = findViewById(R.id.signupB);
+        gSignB = findViewById(R.id.g_signB);
+
+        progressDialog = new Dialog(LoginActivity.this);
+        progressDialog.setContentView(R.layout.dialog_layout);
+        progressDialog.setCancelable(false);
+        progressDialog.getWindow().setLayout(ViewGroup.LayoutParams.WRAP_CONTENT,ViewGroup.LayoutParams.WRAP_CONTENT);
+
+        dialogText = progressDialog.findViewById(R.id.dialog_text);
+        dialogText.setText("Signing in ...");
+
+        mAuth = FirebaseAuth.getInstance();
+
+//        signInRequest = BeginSignInRequest.builder()
+//                .setGoogleIdTokenRequestOptions(BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
+//                        .setSupported(true)
+//                        // Your server's client ID, not your Android client ID.
+//                        .setServerClientId(getString(R.string.default_web_client_id))
+//                        // Only show accounts previously used to sign in.
+//                        .setFilterByAuthorizedAccounts(true)
+//                        .build())
+//                .build();
+
+
+        loginB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if(validateDate())
+                {
+                    login();
+                }
+
+            }
+        });
+
+        signupB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this,SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        gSignB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                googleSignIn();
+            }
+        });
+
+    }
+
+    private boolean validateDate()
+    {
+
+        if(email.getText().toString().isEmpty())
+        {
+            email.setError("Enter E-Mail ID");
+            return false;
+        }
+
+        if(pass.getText().toString().isEmpty())
+        {
+            pass.setError("Enter Password");
+            return false;
+        }
+        return true;
+    }
+
+    private void login()
+    {
+        progressDialog.show();
+
+        mAuth.signInWithEmailAndPassword(email.getText().toString().trim(), pass.getText().toString().trim())
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+
+                            Toast.makeText(LoginActivity.this,"Login Success", Toast.LENGTH_SHORT).show();
+                            progressDialog.dismiss();
+                            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                            startActivity(intent);
+                            finish();
+
+                        } else {
+                            progressDialog.dismiss();
+                            Toast.makeText(LoginActivity.this, task.getException().getMessage(),
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    private void googleSignIn()
+    {
+
+    }
+}
---
+++ app/src/main/java/com/example/quizup/MainActivity.java
@@ -0,0 +1,98 @@
+package com.example.quizup;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Menu;
+import android.widget.FrameLayout;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.navigation.NavigationView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.quizup.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
+    private FrameLayout main_frame;
+
+    private AppBarConfiguration mAppBarConfiguration;
+    private ActivityMainBinding binding;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener OnNavigationItemSelectedListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+//                    switch(menuItem.getItemId())
+//                    {
+//                        case R.id.nav_home :
+//                            setFragment(new CategoryFragment());
+//                            return true;
+//
+//                        case R.id.nav_leaderboard :
+//                            setFragment(new LeaderboardFragment());
+//                            return true;
+//
+//                        case R.id.nav_account:
+//                            setFragment(new AccountFragment());
+//                            return true;
+//
+//                    }
+
+                    return false;
+                }
+            };
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        bottomNavigationView = findViewById(R.id.bottom_nav_bar);
+        main_frame = findViewById(R.id.main_frame);
+        bottomNavigationView.setOnNavigationItemSelectedListener(OnNavigationItemSelectedListener);
+
+        DrawerLayout drawer = binding.drawerLayout;
+        NavigationView navigationView = binding.navView;
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
+                .setOpenableLayout(drawer)
+                .build();
+
+        setFragment(new CategoryFragment());
+
+//        NavController navController = Navigation.findNavController(this, R.id.nav_view);
+//        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+//        NavigationUI.setupWithNavController(navigationView, navController);
+    }
+
+    private void setFragment(Fragment fragment)
+    {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(main_frame.getId(),fragment);
+        transaction.commit();
+
+
+    }
+
+
+
+
+}
